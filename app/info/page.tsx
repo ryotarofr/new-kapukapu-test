fe6362246c80f15678ffdb16e11632173aa1eb90
ryotarofr <ryoryo.fr0608@icloud.com>
2023-07-24 15:07:33 +0900

    test

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,22 +1,32 @@
 
+import getContentsByUserId from '@/actions/getContentsByUserId';
 import Infomation from '@/components/Infomation';
+import Sidebar from '@/components/Sidebar';
 import getPostMetadata from '@/utils/getPostMetadata';
 
 
 
-const Info = () => {
+const Info = async () => {
+  const userContents = await getContentsByUserId()
 
   const postMetadata = getPostMetadata();
   const infoPreviews = postMetadata.map((post) => (
     <Infomation key={post.slug} {...post} />
   ));
   return (
-    <div className="">
-      <div className="text-sm ml-2 mb-4">全てのお知らせ</div>
-      <div
-        className="grid grid-cols-1 xl:grid-cols-2 gap-4"
-      >
-        {infoPreviews}
+    <div className="
+    grid grid-cols-1 gap-2
+    md:grid md:grid-cols-12 md:gap-8">
+      <div className="col-span-3">
+        <Sidebar contents={userContents} />
+      </div>
+      <div className='col-span-6'>
+        <div className="text-sm ml-2 my-6">全てのお知らせ</div>
+        <div
+          className="grid grid-cols-1 xl:grid-cols-2 gap-4"
+        >
+          {infoPreviews}
+        </div>
       </div>
     </div>
   )
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -4,6 +4,8 @@ import matter from "gray-matter";
 
 import getPostMetadata from "@/utils/getPostMetadata";
 import PostContent from "@/components/PostContent";
+import Sidebar from "@/components/Sidebar";
+import getContentsByUserId from "@/actions/getContentsByUserId";
 
 export const revalidate = 60
 
@@ -23,41 +25,49 @@ export const generateStaticParams = async () => {
   }));
 };
 
-const PostPage = (props: any) => {
+const PostPage = async (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
+
+  const userContents = await getContentsByUserId()
 
   return (
     <div>
       {post.data.subrcrive && (
         <PostContent>
-          <div>
-            <div className="my-12 text-center">
-              <h1 className="text-2xl text-slate-700 dark:text-slate-300 ">{post.data.title}</h1>
-              <p className="text-slate-600 dark:text-slate-400 mt-2">{post.data.date}</p>
-            </div>
+          <div className='lg:flex'>
+            <Sidebar contents={userContents} />
+            <div className="mx-2">
+              <div className="my-12 text-center">
+                <h1 className="pt-6 text-2xl text-slate-700 dark:text-slate-300 ">{post.data.title}</h1>
+                <p className="text-slate-600 dark:text-slate-400 mt-2">{post.data.date}</p>
+              </div>
 
-            <article
-              className="mx-auto max-w-4xl prose prose-rose dark:prose-cyan prose-h2:text-indigo-800 prose-h1:text-indigo-950 dark:prose-h2:text-indigo-100 dark:prose-h3:text-indigo-100 prose-h3:text-indigo-800 prose-p:text-slate-900 dark:prose-p:text-indigo-50 text-slate-900 dark:text-white"
-            >
-              <Markdown>{post.content}</Markdown>
-            </article>
+              <article
+                className="mx-auto max-w-4xl prose prose-rose dark:prose-cyan prose-h2:text-indigo-800 prose-h1:text-indigo-950 dark:prose-h2:text-red-400 dark:prose-h3:text-indigo-100 prose-h3:text-indigo-800 prose-p:text-slate-900 dark:prose-p:text-indigo-50 text-slate-900 dark:text-white"
+              >
+                <Markdown>{post.content}</Markdown>
+              </article>
+            </div>
           </div>
         </PostContent>
 
       )}
       {!post.data.subrcrive && (
-        <div>
-          <div className="my-12 text-center">
-            <h1 className="text-2xl text-slate-700 dark:text-slate-300 ">{post.data.title}</h1>
-            <p className="text-slate-600 dark:text-slate-400 mt-2">{post.data.date}</p>
-          </div>
+        <div className='lg:flex'>
+          <Sidebar contents={userContents} />
+          <div className="mx-2">
+            <div className="my-12 text-center">
+              <h1 className="pt-6 text-2xl text-slate-700 dark:text-slate-300 ">{post.data.title}</h1>
+              <p className="text-slate-600 dark:text-slate-400 mt-2">{post.data.date}</p>
+            </div>
 
-          <article
-            className="mx-auto max-w-4xl prose prose-rose dark:prose-cyan prose-h2:text-indigo-800 prose-h1:text-indigo-950 dark:prose-h2:text-indigo-100 dark:prose-h3:text-indigo-100 prose-h3:text-indigo-800 prose-p:text-slate-900 dark:prose-p:text-indigo-50 text-slate-900 dark:text-white"
-          >
-            <Markdown>{post.content}</Markdown>
-          </article>
+            <article
+              className="mx-auto max-w-4xl prose prose-rose dark:prose-cyan prose-h1:text-indigo-950 dark:prose-h1:text-sky-500 prose-h2:text-indigo-800 dark:prose-h2:text-sky-300 dark:prose-h3:text-sky-300 prose-h3:text-indigo-800 dark:prose-h4:text-sky-200 prose-h4:text-indigo-800 prose-p:text-slate-900 dark:prose-p:text-indigo-50 dark:prose-strong:font-bold dark:prose-strong:text-orange-300 text-slate-900 dark:text-white"
+            >
+              <Markdown>{post.content}</Markdown>
+            </article>
+          </div>
         </div>
       )}
     </div>
